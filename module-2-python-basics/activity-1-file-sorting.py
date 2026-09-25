7900de77fedcddeedf64957b3d9db0d75c1c2741
Chxxrz <jbpecson3@gmail.com>
2026-09-25 14:18:22 +0000

    update

--- module-2-python-basics/activity-1-file-sorting.py
+++ module-2-python-basics/activity-1-file-sorting.py
@@ -1,7 +1,7 @@
 """
 Module 2 — Activity: File Sorting with os and shutil
-Student: [your name]
-Date: [date]
+Student: John Brian O. Pecson
+Date: 9/25/26
 
 ============================================
 WHAT DID YOU BUILD? (explain in your own words)
