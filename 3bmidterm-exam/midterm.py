4a3e34b1eff5ece328df0f376ed09722003c57cf
Chxxrz <jbpecson3@gmail.com>
2026-09-26 04:03:10 +0000

    1st Try

---
+++ 3bmidterm-exam/midterm.py
@@ -0,0 +1,56 @@
+"""
+Midterm Practical Exam — Movie Collection Manager
+Student: John Brian O. Pecson
+"""
+
+movies = []
+
+
+def display_menu():
+    print("=== Movie Collection Manager ===")
+    print("1. Add a Movie")
+    print("2. View all movies")
+    print("3. Count watched vs unwatched")
+    print("4. Find a movie")
+    print("5. Exit")
+    print(input("Choose an option: "))
+    # print the menu
+    # return the user's choice
+    pass
+
+
+def add_movie(movie_list):
+    # ask for title, director, and status
+    # build the movie string
+    # add it to the list
+    pass
+
+
+def view_movies(movie_list):
+    # loop through and print every movie
+    # handle empty list
+    pass
+
+
+def count_watched_unwatched(movie_list):
+    # loop through the list
+    # count Watched vs Unwatched
+    # return both counts
+    pass
+
+
+def find_movie(movie_list):
+    # ask for a movie title
+    # search the list
+    # search should be case-insensitive
+    # print the result or "Movie not found."
+    pass
+
+
+def main():
+    # create the main menu loop
+    # call the appropriate function based on the user's choice
+    pass
+
+
+main()
